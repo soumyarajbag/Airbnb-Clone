bbb8d7003097a996387436b5216691d5e80a7094
soumyarajbag <soumyarajbag@gmail.com>
2023-11-02 00:56:33 +0530

    first commit

---
+++ components/common/Navbar.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+   <div>
+    <div>
+
+    </div>
+    <div>
+
+    </div>
+    <div>
+        
+    </div>
+   </div>
+  )
+}
+
+export default Navbar
